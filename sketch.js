1972e86fc12a26728a5cb593aafb033ab1863e59
Surya Dantuluri <surya.mvhs@gmail.com>
2018-01-13 15:58:55 -0800

    javascript refresh

---
+++ sketch.js
@@ -0,0 +1,29 @@
+var price = [0];
+
+function setup() {
+    fetchData();
+}
+
+
+
+function fetchData() { //  create a loop function
+    i = 1;
+    setTimeout(function () { //  call a 3s setTimeout when the loop is called
+        loadJSON("https://api.coinmarketcap.com/v1/ticker/?start=0&limit=1", gotData);
+        //  your code here
+        i++; //  increment the counter
+        if (i > 0) { //  if the counter < 10, call the loop function
+            fetchData(); //  ..  again which will trigger another 
+        } //  ..  setTimeout()
+    }, 15000)
+}
+
+function gotData(data) {
+    print(data);
+    price.push(data[0].price_usd);
+    console.log(data[0].price_usd);
+
+}
+
+
+function draw() {}
