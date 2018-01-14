cfdbad051004d91ccb6a9619beee44f6775656bb
Surya Dantuluri <surya.mvhs@gmail.com>
2018-01-13 17:56:13 -0800

    dynamic

--- sketch.js
+++ sketch.js
@@ -10,22 +10,47 @@ function fetchData() { //  create a loop function
     console.log("fetch data");
     i = 1;
     setTimeout(function () { //  call a 3s setTimeout when the loop is called
-        loadJSON("https://api.coinmarketcap.com/v1/ticker/?start=0&limit=1", gotData);
+        loadJSON("https://api.coinmarketcap.com/v1/ticker/?start=0&limit=20", gotData);
         //  your code here
         i++; //  increment the counter
         if (i > 0) { //  if the counter < 10, call the loop function
+
+
+
+
+
+
+
+
+
             fetchData(); //  ..  again which will trigger another 
+
+
+
+
+
         } //  ..  setTimeout()
     }, 6000)
 }
 
+
+
 function gotData(data) {
+
     print(data);
-    price.push(data[0].price_usd);
-    if (price[price.length - 2] < price[price.length - 1])
-        console.log("BITCOIN HAS GONE UP!");
-    console.log(data[0].price_usd);
-    document.getElementById("bitcoindemo").innerHTML = data[0].price_usd;
+    for (int count = 0; count < data.length; count++) {
+
+        price.push(data[0].price_usd);
+        if (price[price.length - 2] < price[price.length - 1])
+            console.log("BITCOIN HAS GONE UP!");
+        console.log(data[0].price_usd);
+        document.getElementById("number" + ).innerHTML = data[0].price_usd;
+
+    }
+
+
+
+
 }
 
 
